db6ccd41f408450260f1d6139f9015b49f3c2723
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-09-02 16:16:08 -0400

    display input values in UI

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 const saveBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
-const leadsList = document.getElementById("ul-el");
+const ulEl = document.getElementById("ul-el");
 
 saveBtn.addEventListener("click", saveFunc);
 inputEl.addEventListener("input", inputFunc);
 
-const myLeads = ["www.google.com", "www.awesome-fond.com"];
+const myLeads = [];
 
 function inputFunc() {}
 
 function saveFunc() {
   let newLead = inputEl.value;
   myLeads.push(newLead);
+  renderLeads();
   console.log(myLeads);
+  inputEl.value = "";
 }
 
-for (let i = 0; i < myLeads.length; i++) {
-  // console.log(myLeads[i]);
-  let li = document.createElement("li");
-  leadsList.appendChild(li).textContent = myLeads[i];
-  //or below code does the same for rendering the list of links
-  // leadsList.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = [];
+
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 }
-
-const div = document.querySelector(".card");
-
-let btn = document.createElement("button");
-div.appendChild(btn).textContent = "buy";
-
-btn.addEventListener("click", function () {
-  let parag = document.createElement("p");
-  div.appendChild(parag).textContent = "Thank you for buying!";
-});
